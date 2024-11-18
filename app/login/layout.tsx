a99d8604f3521d5d62e6efbe59777a98005b8008
forloop7533 <forloopcontent@gmail.com>
2024-11-18 10:28:35 +0500

    added invoices, home page

---
+++ app/login/layout.tsx
@@ -0,0 +1,7 @@
+export default async function Layout(){
+    return (
+        <main>
+            <h6>This page doesnt exist yet :/</h6>
+        </main>
+    )
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default async function page() {
+    return (
+        <main>
+            <h1>this page doesnt exist yet :/</h1>
+        </main>
+    )
+}
